d782cb85c10065a5d2df43d6c3eb00de6c48c326
AMILIEN Antoine <amilienantoine.dev@gmail.com>
2023-10-11 14:23:02 +0200

    update api getDispoBar with fakeData

--- src/pages/api/nxoCallingStatusCollect/v1.js
+++ src/pages/api/nxoCallingStatusCollect/v1.js
@@ -24,20 +24,22 @@ export default async function handler(req, res) {
 
     var statusForThisService = {
       indispo: {
-        lastTwentyFourHours: getNumberOfServiceIsIndispoForLast24Hours(
+        lastTwentyFourHours: await getNumberOfServiceIsIndispoForLast24Hours(
           SERVICE.offerName
         ),
-        lastThirtyDays: getNumberOfServiceIsIndispoForLast30Days(
+        lastThirtyDays: await getNumberOfServiceIsIndispoForLast30Days(
           SERVICE.offerName
         ),
         lastThreeHundredAndSixtyFiveDays:
-          getNumberOfServiceIsIndispoForLast365Days(SERVICE.offerName),
+          await getNumberOfServiceIsIndispoForLast365Days(SERVICE.offerName),
         lastMonth: getNumberOfServiceIsIndispoForLastMonth(SERVICE.offerName),
       },
+      graph: await getDispoBar(SERVICE.offerName),
     };
 
     res.status(200).json({ statusForThisService: statusForThisService });
-  } catch {
+  } catch (_error) {
+    console.log(_error);
     res.status(500).json({ error: "Internal Server Error." }, { status: 500 });
   }
 }
@@ -47,12 +49,12 @@ export default async function handler(req, res) {
 Domain functions
 *
 */
-function getNumberOfServiceIsIndispoForLast24Hours(offerName) {
+async function getNumberOfServiceIsIndispoForLast24Hours(offerName) {
   //sql request : SELECT SUM(indispo) AS indispo FROM nxo_calling nc WHERE service_categorie ='NXO' AND offre_name IN ('NXO','TEAMS-CALLING') AND to_timestamp(timestamp_utc_s) >= NOW() - INTERVAL '24h'
   return 0;
 }
 
-function getNumberOfServiceIsIndispoForLast30Days(offerName) {
+async function getNumberOfServiceIsIndispoForLast30Days(offerName) {
   //sql request :
   /*
 SELECT SUM(indispo) AS indispo
@@ -65,7 +67,7 @@ AND to_timestamp(timestamp_utc_s) >= NOW() - INTERVAL '30D'
   return 0;
 }
 
-function getNumberOfServiceIsIndispoForLast365Days(offerName) {
+async function getNumberOfServiceIsIndispoForLast365Days(offerName) {
   //sql request :
   /*
 SELECT SUM(indispo) AS indispo
@@ -78,7 +80,7 @@ AND to_timestamp(timestamp_utc_s) >= NOW() - INTERVAL '365D'
   return 0;
 }
 
-function getNumberOfServiceIsIndispoForLastMonth(offerName) {
+async function getNumberOfServiceIsIndispoForLastMonth(offerName) {
   //sql request :
   /*
 SELECT SUM(indispo) AS indispo
@@ -89,4 +91,285 @@ AND offre_name in ('NXO','TEAMS-CALLING')
 AND DATE_TRUNC('month', TO_TIMESTAMP(timestamp_utc_s)) = ADD_MONTHS(DATE_TRUNC('month', NOW()), -1)
 */
   return 0;
+}
+
+const fakeData = [
+  {
+    ts: 1696197600,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696197600,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696197600,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696197600,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696284000,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696284000,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696284000,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696284000,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696456800,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696456800,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696456800,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696456800,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696543200,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696543200,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696543200,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696543200,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696629600,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696629600,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696629600,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696629600,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696716000,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696716000,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696716000,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696716000,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696802400,
+    offre_name: "NXO",
+    service_categorie: "NXO",
+    service_name: "Service de téléphonie opérateur SIP",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696802400,
+    offre_name: "NXO",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie public",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696802400,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "NXO",
+    service_name: "Service SIP d interco Cloud",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+  {
+    ts: 1696802400,
+    offre_name: "TEAMS-CALLING",
+    service_categorie: "Ressource distante",
+    service_name: "Service Telephonie Microsoft",
+    Disponibilite: 100,
+    indispo: 0,
+    maintenance: 0,
+  },
+];
+//TODO Revoir la nom de cette fonction
+async function getDispoBar(offerName) {
+  // Fonction pour formater le nom du service en camelCase
+  function formatServiceName(service_name) {
+    return service_name
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]+(.)/g, (match, chr) => chr.toUpperCase());
+  }
+
+  //Exect du sql et retourne les data (ici fakeDate)
+  //1 Je veux les repartirs dans 4 tableaux en fonction de leur service_name (attention rien a voir avec le service_name dans ma env var)
+  const serviceGroups = {};
+
+  for await (let item of fakeData) {
+    const formattedServiceName = formatServiceName(item.service_name);
+
+    // Vérifiez si le tableau pour ce service_name existe, sinon, créez-le
+    if (!serviceGroups[formattedServiceName]) {
+      serviceGroups[formattedServiceName] = [];
+    }
+
+    // Ajoutez l'élément au tableau approprié
+    serviceGroups[formattedServiceName].push(item);
+  }
+  return serviceGroups;
 }
